36a62eed5501adfca28b64967914ffe8600746b2
suraj0208 <surajkumargunjal@gmail.com>
2017-03-03 11:56:22 +0530

    Basic vRouter simulation

--- src/program/first_app/first_app.lua
+++ src/program/first_app/first_app.lua
@@ -1,20 +1,121 @@
 module(..., package.seeall)
 
-local pcap = require("apps.pcap.pcap");
-local basic = require("apps.basic.basic_apps");
+local pcap = require("apps.pcap.pcap")
+local raw = require("apps.socket.raw")
+local packet = require("core.packet")
+local link = require("core.link")
+local ethernet = require("lib.protocol.ethernet")
+local ipv4 = require("lib.protocol.ipv4")
+local datagram = require("lib.protocol.datagram")
+local constants = require("apps.lwaftr.constants")
+local ffi = require("ffi")
+local lwtypes = require("apps.lwaftr.lwtypes")
+
+local cast = ffi.cast
+local ethHeaderSize = constants.ethernet_header_size
+local ethernet_header_ptr_type = lwtypes.ethernet_header_ptr_type
+
+
+function write_eth_header(dst_ptr, ether_src, ether_dst, eth_type)
+	local eth_hdr = cast(ethernet_header_ptr_type, dst_ptr)
+	eth_hdr.ether_shost = ether_src
+	eth_hdr.ether_dhost = ether_dst
+	eth_hdr.ether_type = eth_type
+end
+
+Sender = {}
+
+function Sender:new()
+	local data = {"a"}
+	return setmetatable({data = data},{__index=Sender})	
+end
+
+function Sender:pull()
+	local src_ip = ipv4:pton("192.168.0.11")
+	local dest_ip = ipv4:pton("192.168.0.14")
+	local src_mac = ethernet:pton("00:00:00:00:00:00")
+	local dest_mac = ethernet:pton("00:00:00:00:00:00")
+	
+	local p = packet.allocate()
+	local dgram = datagram:new(p)
+	local ipv4_header = ipv4:new({ttl = constants.default_ttl,
+	protocol = constants.proto_icmp,
+	src = src_ip, dst = dest_ip})
+	dgram:push(ipv4_header)
+	p = dgram:packet()
+	ipv4_header:free()
+	p = packet.shiftright(p, ethHeaderSize)
+	write_eth_header(p.data, src_mac, dest_mac, constants.n_ethertype_ipv4)	 
+	
+	link.transmit(self.output.output , p)			
+end
+
+Receiver = {}
+
+function Receiver:new()
+	local data = {"a"}
+	return setmetatable( {packets = {}}, { __index=Receiver}) 
+end
+
+function Receiver:push()
+	local l = self.input.input
+	if not link.empty(l) then
+		local p = link.receive(l)
+		table.insert(self.packets,p);
+	end
+end
+
+function Receiver:pull()
+	local npackets = #self.packets;
+	local p = self.packets[1];
+	if p then 		
+		link.transmit(self.output.output,p);
+	end
+end
 
 function run (parameters)
-	local input_file = "src/program/example_replay/input.pcap";
+	if not (#parameters == 1) then
+		print("Usage: first_app <interface>")
+		main.exit(1)
+	end
 
-	local c = config.new();
-	config.app(c,"capture",pcap.PcapReader, input_file);
-	config.app(c,"repeater",basic.Repeater);
-	config.app(c,"writer",pcap.PcapWriter, "src/program/first_app/output.pcap");
-	
-	config.link(c,"capture.output -> repeater.input");
-	config.link(c,"repeater.output -> writer.input");
+	local interface = parameters[1]
 
-	engine.configure(c);
-	engine.main( {duration=1, report = {showlinks=true}} )
+	print (parse_cidr_ipv4("192.168.15.15/32"));
+	local c = config.new()
+	config.app(c, "sender", Sender)
+	config.app(c, "playback", raw.RawSocket, interface)
+	config.app(c, "receiver",Receiver);
+
+	config.link(c, "sender.output -> receiver.input")
+	config.link(c, "receiver.output -> playback.rx");
+	engine.configure(c)
+	engine.main({duration=0.01, report = {showlinks=true}})
 end
-	
+
+function parse_cidr_ipv4 (cidr)
+	local address, prefix_size =  string.match(cidr, "^(.+)/(%d+)$")
+	if not ( address and prefix_size ) then
+		return false, "malformed IPv4 CIDR: " .. tostring(cidr)
+	end
+	prefix_size = tonumber(prefix_size)
+	if prefix_size > 32 then
+		return false, "IPv6 CIDR mask is too big: " .. prefix_size
+	end
+	if prefix_size == 0 then
+		return true -- any IP
+	end
+	local in_addr  = ffi.new("int32_t[1]")
+	local AF_INET = 2;
+	local result = ffi.C.inet_pton(AF_INET, address, in_addr)
+	if result ~= 1 then
+		return false, "malformed IPv4 address: " .. address
+	end
+	if prefix_size == 32 then
+		-- single IP address
+		return true, in_addr[0]
+	end
+	local mask = bit.bswap(bit.bnot(bit.rshift(bit.bnot(0), prefix_size)))
+	print (in_addr[0]);
+	return true, bit.band(in_addr[0], mask), mask
+end
---
+++ src/program/gre_demo/gre_demo.lua
@@ -0,0 +1,261 @@
+-- OpenContrail vRouter implementation in user space
+module(..., package.seeall)
+
+local ffi = require("ffi")
+
+local pcap = require("apps.pcap.pcap")
+
+local ethernet = require("lib.protocol.ethernet")
+local datagram = require("lib.protocol.datagram")
+local ipv4 = require("lib.protocol.ipv4")
+
+local link = require("core.link")
+local packet = require("core.packet")
+local lib = require("core.lib")
+
+local lwutil = require("apps.lwaftr.lwutil")
+local lwtypes = require("apps.lwaftr.lwtypes")
+
+local transmit, receive = link.transmit, link.receive
+local rd16, rd32, wr16 = lwutil.rd16, lwutil.rd32, lwutil.wr16
+local ethernet_header_ptr_type = lwtypes.ethernet_header_ptr_type
+local cast = ffi.cast
+
+-- Dummy Values
+local from_ip = ipv4:pton("192.168.0.2")
+local to_ip = ipv4:pton("192.168.0.12")
+local from_eth = ethernet:pton("01:02:03:04:05:06")
+local to_eth = ethernet:pton("06:05:04:03:02:01")
+
+-- Constants
+local ethernet_header_size = 14
+
+local o_ethernet_dst_addr = 0
+local o_ethernet_src_addr = 6
+local o_ethernet_ethertype = 12
+
+local o_ipv4_checksum = 10
+local o_ipv4_src_addr = 12
+local o_ipv4_dst_addr = 16
+
+local n_ethertype_ipv4 = 0x0800
+
+local default_ttl = 255
+
+local proto_icmp = 1
+local proto_tcp = 6
+
+-- Basic Functions
+
+local function get_ethertype(pkt)
+   return rd16(pkt.data + (ethernet_header_size - 2))
+end
+
+local function get_ethernet_payload(pkt)
+   return pkt.data + ethernet_header_size
+end
+
+local function get_ipv4_dst_address(ptr)
+   return rd32(ptr + o_ipv4_dst_addr)
+end
+
+local function get_ipv4_src_ptr(ptr)
+   return ptr + o_ipv4_src_addr
+end
+
+local function get_ipv4_src_address(ptr)
+   return rd32(get_ipv4_src_ptr(ptr))
+end
+
+local function get_ipv4_checksum_ptr (ptr)
+   return ptr + o_ipv4_checksum
+end
+
+local function copy_ether(dst, src)
+   ffi.copy(dst, src, 6)
+end
+
+local function copy_ipv4(dst, src)
+   ffi.copy(dst, src, 4)
+end
+
+function write_eth_header(dst_ptr, ether_src, ether_dst, eth_type)
+   local eth_hdr = cast(ethernet_header_ptr_type, dst_ptr)
+   eth_hdr.ether_shost = ether_src
+   eth_hdr.ether_dhost = ether_dst
+   eth_hdr.ether_type = eth_type
+end
+
+-- Code for Basic VM app
+
+VM = {
+	config = {
+		--mac_address = { required = true },
+		ipv4_address = { required = true },
+		dst_ipv4_address = { required = true }
+	}
+}
+
+function VM:new(conf)
+	--local mac_address = ethernet:pton(conf.mac_address)
+	local ipv4_address = ipv4:pton(conf.ipv4_address)
+	local dst_ipv4_address = ipv4:pton(conf.dst_ipv4_address)
+
+	local config = {
+		--mac_address = mac_address,
+		ipv4_address = ipv4_address,
+		dst_ipv4_address = dst_ipv4_address
+	}
+
+	return setmetatable({config = config}, { __index = VM })
+end
+
+function VM:push()
+	local input = self.input.input
+
+	if input then
+		for _ = 1, link.nreadable(input) do
+			local pkt = receive(input)
+			local ipv4_pkt = get_ethernet_payload(pkt)
+			local dst_ip = get_ipv4_dst_address(ipv4_pkt)
+			local src_addr = (ipv4_pkt + 12)[0] .. "." .. (ipv4_pkt + 12)[1] .. "." .. (ipv4_pkt + 12)[2] .. "." .. (ipv4_pkt + 12)[3] .. ""
+			local addr = (ipv4_pkt + 16)[0] .. "." .. (ipv4_pkt + 16)[1] .. "." .. (ipv4_pkt + 16)[2] .. "." .. (ipv4_pkt + 16)[3] .. ""
+			
+			local sr_ip =ipv4:ntop(self.config.ipv4_address)
+			--print("curent " .. sr_ip);
+			--print("dst " .. addr);
+
+			--print((dst_addr))
+			if tostring(addr) == tostring(sr_ip) then
+				print(("Packet received for IP address: %s"):format(src_addr))
+			else
+				packet.free(pkt)
+			end 	
+		end
+	end
+end
+
+function VM:pull()
+	
+	local output = self.output.output
+
+	local new_packet = packet.allocate()
+	local dgram = datagram:new(new_packet)
+	local ipv4_header = ipv4:new({ ttl = default_ttl,
+								   protocol = proto_icmp,
+								   src = self.config.ipv4_address,
+								   dst = self.config.dst_ipv4_address })
+
+	dgram:push(ipv4_header)
+	new_packet = dgram:packet()
+	new_packet = packet.shiftright(new_packet, ethernet_header_size)
+	write_eth_header(new_packet.data, from_eth, to_eth, n_ethertype_ipv4)	
+
+	local payl = get_ethernet_payload(new_packet)
+	local new_dst = (payl + 16)[0] .. "." .. (payl + 16)[1] .. "." .. (payl + 16)[2] .. "." .. (payl + 16)[3] .. "" 
+	if output then
+		for _ = 1, link.nwritable(output) do
+			transmit(output, new_packet)
+		end	
+	end
+end
+
+
+
+-- vRouter App with simple forwarding
+
+vRouter = {}
+
+function vRouter:new(conf)
+	local rtable = conf.ip_addresses
+	return setmetatable({ rtable = rtable }, { __index = vRouter }) 
+end
+
+function vRouter:add_entry(ip, link, i)
+	table.insert(self.output, i)
+	table.insert(self.rtable, {ip, self.output.i})
+end
+
+function vRouter:push()
+	local input = self.input.input
+
+	
+	if input then
+		for _ = 1, link.nreadable(input) do
+			local pkt = receive(input)
+			local ipv4_pkt = get_ethernet_payload(pkt)
+			local dst_ip = get_ipv4_dst_address(ipv4_pkt)
+			local src_addr = (ipv4_pkt + 12)[0] .. "." .. (ipv4_pkt + 12)[1] .. "." .. (ipv4_pkt + 12)[2] .. "." .. (ipv4_pkt + 12)[3] .. ""
+			local addr = (ipv4_pkt + 16)[0] .. "." .. (ipv4_pkt + 16)[1] .. "." .. (ipv4_pkt + 16)[2] .. "." .. (ipv4_pkt + 16)[3] .. ""
+			
+			--print("src = " .. src_addr)
+			--print("recvd" .. addr)	
+			--[[
+			if(src_addr == "192.168.0.11") then
+				print("src  " .. src_addr)
+				print("recvd " .. addr)
+			end
+			--]]
+
+
+			--if get_ethertype(pkt) == n_ethertype_ipv4 then
+				for i,ip in ipairs(self.rtable) do
+					--transmit(self.output["4"], pkt)
+					if ip == addr then
+						transmit(self.output[tostring(i)], pkt)
+						break
+					end
+				end	
+			---else
+				--packet.free(pkt)
+			--end
+		end
+	end
+end
+
+
+-- Main Function
+
+function run()
+	local c = config.new()
+
+	local arr_ip = {}
+
+	for i = 1,10 do
+		local ip = "192.168.0." .. i
+		table.insert(arr_ip, ip)
+		config.app(c, "vm" .. i, VM, { ipv4_address = ip, dst_ipv4_address = "192.168.0." .. (i+1)})
+	end
+
+	local pcap_file = "/home/suraj/workspace/BEProject/snabb/src/program/example_replay/http_m1.pcap"
+
+	config.app(c, "vRouter", vRouter, { ip_addresses = arr_ip })
+	config.app(c, "capture", pcap.PcapReader, pcap_file)
+
+
+	config.link(c, "capture.output -> vRouter.input")
+
+	--config.app(c, "vm11", VM, { ipv4_address = "192.168.0.11", dst_ipv4_address = "192.168.0.4" })	
+	--config.link(c, "vm11.output -> vRouter.input")	
+	
+	--[[
+	config.link(c, "vRouter.".. "1" .." -> vm" .. 1 .. ".input")	
+	config.link(c, "vRoute".. 2 .." -> vm" .. 2 .. ".input")	
+	config.link(c, "vRoute".. 3 .." -> vm" .. 3 .. ".input")	
+	config.link(c, "vRoute".. 4 .." -> vm" .. 4 .. ".input")	
+	config.link(c, "vRouter.".. 5 .." -> vm" .. 5 .. ".input")	
+	config.link(c, "vRoute".. 6 .." -> vm" .. 6 .. ".input")	
+	config.link(c, "vRoute".. 7 .." -> vm" .. 7 .. ".input")	
+	config.link(c, "vRoute".. 8 .." -> vm" .. 8 .. ".input")	
+	config.link(c, "vRoute".. 9 .." -> vm" .. 9 .. ".input")	
+	config.link(c, "vRoute".. 10 .." -> vm" .. 10 .. ".input")	
+	--]]
+
+	for i = 1,10 do
+		config.link(c, "vRouter.".. tostring(i) .." -> vm" .. i .. ".input")
+	end
+
+	engine.configure(c)
+
+	engine.main({ duration = 0.00001 , report = { showlinks = true } })
+end
